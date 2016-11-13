b69e62db27c64ed7579948d1791d9011492b6980
Bundit J <Bundit J>
2016-11-13 21:51:04 +0700

    test

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
-	"os"
-	"fmt"
-	"html"
-	"log"
-	"net/http"
+  "os"
+  "fmt"
+  "html"
+  "log"
+  "net/http"
+  "github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+  // Using net/http
+  //http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+  //	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+  //})
+  //log.Fatal(http.ListenAndServe(":" + os.Getenv("HTTP_PLATFORM_PORT"), nil))
 
-	log.Fatal(http.ListenAndServe(":" + os.Getenv("HTTP_PLATFORM_PORT"), nil))
-
+  // Using gorilla/mux
+  router := mux.NewRouter().StrictSlash(true)
+  router.HandleFunc("/", Index)
+  log.Fatal(http.ListenAndServe(":" + os.Getenv("HTTP_PLATFORM_PORT"), router))
 }
+
+func Index(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
