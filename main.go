18c356441a6f2a8d89a66d6743d00305623c8dcd
Bundit J <Bundit J>
2016-11-13 21:58:49 +0700

    test

--- main.go
+++ main.go
@@ -7,7 +7,16 @@ import (
   "log"
   "net/http"
   "github.com/gorilla/mux"
+  "time"
 )
+
+type Todo struct {
+  Name      string
+  Completed bool
+  Due       time.Time
+}
+
+type Todos []Todo
 
 func main() {
   // Using net/http
@@ -19,9 +28,26 @@ func main() {
   // Using gorilla/mux
   router := mux.NewRouter().StrictSlash(true)
   router.HandleFunc("/", Index)
+  router.HandleFunc("/todos", TodoIndex)
+  router.HandleFunc("/todos/{todoId}", TodoShow)
+
   log.Fatal(http.ListenAndServe(":" + os.Getenv("HTTP_PLATFORM_PORT"), router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+  todos := Todos{
+    Todo{Name: "Write presentation"},
+    Todo{Name: "Host meetup"},
+  }
+  json.NewEncoder(w).Encode(todos)
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  todoId := vars["todoId"]
+  fmt.Fprintln(w, "Todo show:", todoId)
 }
